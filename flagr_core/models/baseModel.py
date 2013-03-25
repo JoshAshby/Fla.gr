b3f486d5bf6dce2620b3269905a6854918a4c624
JoshAshby <joshuaashby@joshashby.com>
2013-03-25 08:13:46 -0600

    Things be break'n. Working on seeing if I can have datetime fields auto format to a string.

--- flagr_core/models/baseModel.py
+++ flagr_core/models/baseModel.py
@@ -12,17 +12,136 @@ http://joshashby.com
 joshuaashby@joshashby.com
 """
 import config.dbBase as db
+from couchdb.mapping import DocumentMeta, DateTimeField, \
+    Mapping, Document
 
 
-class baseCouchModel(object):
-    """
-    Extension of the couchdb-python Document class to provide a
-    bit more of an object interface with the documents, since some
-    things such as saving and deleteing the documents doesn't feel
-    very object based to me.
-    """
-    def __init__(self, **kwargs):
-        pass
+class baseCouchModel(Mapping):
+    __metaclass__ = DocumentMeta
+
+    def __init__(self, id=None, **values):
+        Mapping.__init__(self, **values)
+        if id is not None:
+            self.id = id
+
+        """
+        Extension of the couchdb-python Document class to provide a
+        bit more of an object interface with the documents, since some
+        things such as saving and deleteing the documents doesn't feel
+        very object based to me.
+        """
+        for attrname, attrval in d.items():
+            if isinstance(attrval, DateTimeField):
+                setattr(self, "formated"+attrname.capitalize(),
+                        datetime.strftime(attrval, "%b %d, %Y"))
+
+
+    def __repr__(self):
+        return '<%s %r@%r %r>' % (type(self).__name__, self.id, self.rev,
+                                  dict([(k, v) for k, v in self._data.items()
+                                        if k not in ('_id', '_rev')]))
+
+    def _get_id(self):
+        if hasattr(self._data, 'id'): # When data is client.Document
+            return self._data.id
+        return self._data.get('_id')
+    def _set_id(self, value):
+        if self.id is not None:
+            raise AttributeError('id can only be set on new documents')
+        self._data['_id'] = value
+    id = property(_get_id, _set_id, doc='The document ID')
+
+    @property
+    def rev(self):
+        """The document revision.
+        
+        :rtype: basestring
+        """
+        if hasattr(self._data, 'rev'): # When data is client.Document
+            return self._data.rev
+        return self._data.get('_rev')
+
+    def items(self):
+        """Return the fields as a list of ``(name, value)`` tuples.
+        
+        This method is provided to enable easy conversion to native dictionary
+        objects, for example to allow use of `mapping.Document` instances with
+        `client.Database.update`.
+        
+        >>> class Post(Document):
+        ...     title = TextField()
+        ...     author = TextField()
+        >>> post = Post(id='foo-bar', title='Foo bar', author='Joe')
+        >>> sorted(post.items())
+        [('_id', 'foo-bar'), ('author', u'Joe'), ('title', u'Foo bar')]
+        
+        :return: a list of ``(name, value)`` tuples
+        """
+        retval = []
+        if self.id is not None:
+            retval.append(('_id', self.id))
+            if self.rev is not None:
+                retval.append(('_rev', self.rev))
+        for name, value in self._data.items():
+            if name not in ('_id', '_rev'):
+                retval.append((name, value))
+        return retval
+
+    @classmethod
+    def load(cls, db, id):
+        """Load a specific document from the given database.
+        
+        :param db: the `Database` object to retrieve the document from
+        :param id: the document ID
+        :return: the `Document` instance, or `None` if no document with the
+                 given ID was found
+        """
+        doc = db.get(id)
+        if doc is None:
+            return None
+        return cls.wrap(doc)
+
+    def store(self, db):
+        """Store the document in the given database."""
+        db.save(self._data)
+        return self
+
+    @classmethod
+    def query(cls, db, map_fun, reduce_fun, language='javascript', **options):
+        """Execute a CouchDB temporary view and map the result values back to
+        objects of this mapping.
+        
+        Note that by default, any properties of the document that are not
+        included in the values of the view will be treated as if they were
+        missing from the document. If you want to load the full document for
+        every row, set the ``include_docs`` option to ``True``.
+        """
+        return db.query(map_fun, reduce_fun=reduce_fun, language=language,
+                        wrapper=cls._wrap_row, **options)
+
+    @classmethod
+    def view(cls, db, viewname, **options):
+        """Execute a CouchDB named view and map the result values back to
+        objects of this mapping.
+        
+        Note that by default, any properties of the document that are not
+        included in the values of the view will be treated as if they were
+        missing from the document. If you want to load the full document for
+        every row, set the ``include_docs`` option to ``True``.
+        """
+        return db.view(viewname, wrapper=cls._wrap_row, **options)
+
+    @classmethod
+    def _wrap_row(cls, row):
+        doc = row.get('doc')
+        if doc is not None:
+            return cls.wrap(doc)
+        data = row['value']
+        data['_id'] = row['id']
+        return cls.wrap(data)
+
+
+
 
     def save(self):
         """
